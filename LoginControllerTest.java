f6d372a3bd91c2a854c0360693c5c260e03ae2ca
tosa03 <127969389+tosa03@users.noreply.github.com>
2025-06-03 22:04:22 +0900

    Update LoginControllerTest.java

--- LoginControllerTest.java
+++ LoginControllerTest.java
@@ -8,18 +8,19 @@ import org.junit.Before;
 import org.junit.Test;
 
 import fw.attribute.Attributes;
+import fw.attribute.Messages;
 import fw.request.WebRequest;
 import univelec.util.DBHelper;
 
 public class LoginControllerTest {
 	DBHelper dbHelper;
 
-	LoginControllerTest controller;
+	LoginController controller;
 
 	@Before
 	public void setUp() throws Exception {
 		dbHelper = new DBHelper();
-		controller = new LoginControllerTest();
+		controller = new LoginController();
 	}
 
 	@After
@@ -33,8 +34,8 @@ public class LoginControllerTest {
 		WebRequest webRequest = new WebRequest(); // ダミー生成（必要に応じてモック）
 		Attributes attributes = new Attributes(); // ダミー生成（必要に応じてモック）
 
-		// テスト対象のインスタンスを生成
-		LoginController controller = new LoginController();
+		// テスト対象のインスタンスを生成→メンバ変数で定義済み
+		//LoginController controller = new LoginController();
 
 		// メソッド呼び出し
 		String actual = controller.loginEntry(webRequest, attributes);
@@ -50,7 +51,18 @@ public class LoginControllerTest {
 
 	@Test
 	public void testLogoutExecute() {
-		fail("まだ実装されていません");
+		// 前処理：ダミーの引数を作る（使わないのでnullでもOK）
+				WebRequest webRequest = new WebRequest(); // ダミー生成（必要に応じてモック）
+				Attributes attributes = new Attributes(); // ダミー生成（必要に応じてモック）
+				// メソッド呼び出し
+				String actual = controller.logoutExecute(webRequest, attributes);
+
+				// 結果検証
+				Assertions.assertThat(actual).isEqualTo("auth/login");
+				// メッセージの検証（"ログアウトしました。" が含まれているか）
+			    Messages messages = attributes.getMessages();
+			    Assertions.assertThat(messages.getInfoMessages())
+			              .contains("ログアウトしました。");
 	}
 
 }
